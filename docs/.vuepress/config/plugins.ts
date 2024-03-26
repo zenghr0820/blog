c3b61db880ff7d086f5f4bf39f945533e36ef148
zenghr <zenghr>
2024-03-27 00:14:17 +0800

    修改algolia爬虫配置文件

--- docs/.vuepress/config/plugins.ts
+++ docs/.vuepress/config/plugins.ts
@@ -23,42 +23,51 @@ export const configPlugins: PluginConfig = [
     appId: process.env.algoliaAppId || "",
     apiKey: process.env.algoliaAppKey || "",
     indexName: "prod_blog",
-    translations: {
-      button: {
-        buttonText: "搜索文档",
-        buttonAriaLabel: "Search"
-      },
-      modal: {
-        searchBox: {
-          resetButtonTitle: "清除查询条件",
-          resetButtonAriaLabel: "Clear the query",
-          cancelButtonText: "关闭",
-          cancelButtonAriaLabel: "Cancel"
+    contextualSearch: true,
+    searchParameters: {
+    },
+    locales: {
+      '/': {
+        lang: 'zh-CN',
+        placeholder: '搜索文档',
+        translations: {
+          button: {
+            buttonText: '搜索文档',
+            buttonAriaLabel: '搜索文档',
+          },
+          modal: {
+            searchBox: {
+              resetButtonTitle: '清除查询条件',
+              resetButtonAriaLabel: '清除查询条件',
+              cancelButtonText: '取消',
+              cancelButtonAriaLabel: '取消',
+            },
+            startScreen: {
+              recentSearchesTitle: '搜索历史',
+              noRecentSearchesText: '没有搜索历史',
+              saveRecentSearchButtonTitle: '保存至搜索历史',
+              removeRecentSearchButtonTitle: '从搜索历史中移除',
+              favoriteSearchesTitle: '收藏',
+              removeFavoriteSearchButtonTitle: '从收藏中移除',
+            },
+            errorScreen: {
+              titleText: '无法获取结果',
+              helpText: '你可能需要检查你的网络连接',
+            },
+            footer: {
+              selectText: '选择',
+              navigateText: '切换',
+              closeText: '关闭',
+              searchByText: '搜索提供者',
+            },
+            noResultsScreen: {
+              noResultsText: '无法找到相关结果',
+              suggestedQueryText: '你可以尝试查询',
+              reportMissingResultsText: '你认为该查询应该有结果？',
+              reportMissingResultsLinkText: '点击反馈',
+            },
+          },
         },
-        startScreen: {
-          recentSearchesTitle: "搜索历史",
-          noRecentSearchesText: "没有搜索历史",
-          saveRecentSearchButtonTitle: "保存至搜索记录",
-          removeRecentSearchButtonTitle: "从搜索历史中移除",
-        },
-        errorScreen: {
-          titleText: "无法找到相关结果",
-          helpText: "您可能需要检查您的网络连接."
-        },
-        footer: {
-          selectText: "选择",
-          selectKeyAriaLabel: "Enter key",
-          navigateText: "切换",
-          navigateUpKeyAriaLabel: "Arrow up",
-          navigateDownKeyAriaLabel: "Arrow down",
-          closeText: "关闭",
-          closeKeyAriaLabel: "Escape key",
-          searchByText: "搜索提供者"
-        },
-        noResultsScreen: {
-          noResultsText: "无法找到相关结果",
-          suggestedQueryText: "你可以尝试查询",
-        }
       }
     }
   }),
